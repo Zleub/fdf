a8c7bd1e907144d9d95d45b220a4882f354dbc89
Zleub <debray.arnaud@gmail.com>
2015-01-01 08:33:55 +0100

    line up

--- src/basics/imagedraw.c
+++ src/basics/imagedraw.c
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/30 11:04:18 by adebray           #+#    #+#             */
-/*   Updated: 2014/12/30 11:57:42 by adebray          ###   ########.fr       */
+/*   Updated: 2015/01/01 08:33:12 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,34 +58,221 @@ void		imagecircle(t_image *image)
 	}
 }
 
-void		imageline(t_image *image)
+int		imageline(t_image *image)
 {
+	int		diagonal = 0;
+
 	if (image->xto != 0)
 		image->width = image->xto - image->x;
 	if (image->yto != 0)
 		image->height = image->yto - image->y;
 	if (image->width < 0 && image->height < 0)
 	{
-		ft_printf("left top\n");
+		diagonal = 1;
 		image->x = image->xto;
 		image->y = image->yto;
 	}
 	else if (image->width < 0 && image->height > 0)
 	{
-		ft_printf("left down\n");
+		diagonal = 2;
 		image->x = image->xto;
 	}
 	else if (image->width > 0 && image->height < 0)
 	{
-		ft_printf("left down\n");
+		diagonal = 3;
 		image->y = image->yto;
 	}
 	else if (image->width > 0 && image->height > 0)
-		ft_printf("right down\n");
+		diagonal = 4;
 	else
 		ft_printf("pixel ?? horizontal && vertical\n");
 	image->width = ft_abs(image->width);
 	image->height = ft_abs(image->height);
+	return (diagonal);
+}
+
+void		longnormaldiagonal(t_image *image)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	*img;
+	int		vars[5];
+
+	ft_printf("long diagonal\n");
+	img = mlx_get_data_addr(image->mlximg, &(vars[0]), &(vars[1]), &(vars[2]));
+	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	vars[3] = image->width / image->height;
+	vars[4] = image->width % image->height;
+	ft_printf("w:%d | h:%d %d::%d\n", image->width, image->height, vars[3], vars[4]);
+	while (i < vars[1])
+	{
+		*(int*)(img + i + (j * vars[1])) = image->color;
+		i += 4;
+		k += 1;
+		l += vars[4];
+		if (l >= image->width)
+		{
+			k -= 1;
+			l -= image->width;
+		}
+		if (k >= vars[3])
+		{
+			k = 0;
+			j += 1;
+		}
+	}
+}
+
+void		highnormaldiagonal(t_image *image)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	*img;
+	int		vars[5];
+
+	ft_printf("high diagonal\n");
+	img = mlx_get_data_addr(image->mlximg, &(vars[0]), &(vars[1]), &(vars[2]));
+	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	vars[3] = image->height / image->width;
+	vars[4] = image->height % image->width;
+	ft_printf("w:%d | h:%d %d::%d\n", image->width, image->height, vars[3], vars[4]);
+	while (i < vars[1])
+	{
+		*(int*)(img + i + (j * vars[1])) = image->color;
+		k += 1;
+		j += 1;
+		l += vars[4];
+		if (l >= image->height)
+		{
+			k -= 1;
+			l -= image->height;
+		}
+		if (k >= vars[3])
+		{
+			k = 0;
+			i += 4;
+		}
+	}
+
+}
+
+void		longinvertdiagonal(t_image *image)
+{
+	ft_printf("long invert diagonal\n");
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	*img;
+	int		vars[5];
+
+	img = mlx_get_data_addr(image->mlximg, &(vars[0]), &(vars[1]), &(vars[2]));
+	i = vars[1] - 4;
+	j = 0;
+	k = 0;
+	l = 0;
+	vars[3] = image->width / image->height;
+	vars[4] = image->width % image->height;
+	// *(int*)(img + i + (j * vars[1])) = image->color;
+	while (i >= 0)
+	{
+		*(int*)(img + i + (j * vars[1])) = image->color;
+		k += 1;
+		l += vars[4];
+		i -= 4;
+		if (l >= image->width)
+		{
+			k -= 1;
+			l -= image->width;
+		}
+		if (k >= vars[3])
+		{
+			k = 0;
+			j += 1;
+		}
+	}
+}
+
+void		highinvertdiagonal(t_image *image)
+{
+	ft_printf("high invert diagonal\n");
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+	char	*img;
+	int		vars[5];
+
+	img = mlx_get_data_addr(image->mlximg, &(vars[0]), &(vars[1]), &(vars[2]));
+	i = vars[1] - 4;
+	j = 0;
+	k = 0;
+	l = 0;
+	vars[3] = image->height / image->width;
+	vars[4] = image->height % image->width;
+	ft_printf("w:%d | h:%d %d::%d\n", image->width, image->height, vars[3], vars[4]);
+	*(int*)(img + i + (j * vars[1])) = image->color;
+	while (i >= 0)
+	{
+		*(int*)(img + i + (j * vars[1])) = image->color;
+		k += 1;
+		j += 1;
+		l += vars[4];
+		if (l >= image->height)
+		{
+			k -= 1;
+			l -= image->height;
+		}
+		if (k >= vars[3])
+		{
+			k = 0;
+			i -= 4;
+		}
+	}
+}
+
+void		imagediagonal(t_image *image, int diagonal)
+{
+	ft_printf("diag: %d\n", diagonal);
+	if (diagonal == 1)
+	{
+		if (image->width >= image->height)
+			longnormaldiagonal(image);
+		else
+			highnormaldiagonal(image);
+	}
+	else if (diagonal == 2)
+	{
+		if (image->width >= image->height)
+			longinvertdiagonal(image);
+		else
+			highinvertdiagonal(image);
+	}
+	else if (diagonal == 3)
+	{
+		if (image->width >= image->height)
+			longinvertdiagonal(image);
+		else
+			highinvertdiagonal(image);
+	}
+	else if (diagonal == 4)
+	{
+		if (image->width >= image->height)
+			longnormaldiagonal(image);
+		else
+			highnormaldiagonal(image);
+		// normaldiagonal(image);
+	}
 }
 
 void		imagedraw(int macro, t_image *current)
@@ -104,9 +291,12 @@ void		imagedraw(int macro, t_image *current)
 	}
 	else if (macro == LIN)
 	{
-		imageline(current);
+		int diagonal = imageline(current);
+		ft_printf("ANANANANAN: %d\n", current->put);
 		if (current->put == 0)
 			manageimage(NEW);
 		// manageimage(SQR);
+		// current->color = current->color / 2;
+		imagediagonal(current, diagonal);
 	}
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/27 14:43:26 by adebray           #+#    #+#             */
-/*   Updated: 2014/12/30 11:56:18 by adebray          ###   ########.fr       */
+/*   Updated: 2015/01/01 08:32:33 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,32 +59,58 @@ int			main(int argc, char **argv)
 	managewindow(NEW);
 
 	image = manageimage(GET);
-	image->color = 0xff0000;
-	image->width = 500;
-	image->height = 500;
-	image->radius = 250;
-	image->x = 0;
-	manageimage(CIR);
-	manageimage(PUT);
+	// image->width = 640;
+	// image->height = 480;
+	// manageimage(NEW);
 
 	image->x = 100;
+	image->y = 250;
+	image->xto = 20;
+	image->yto = 400;
+	image->color = 0xff0000;
+	manageimage(LIN);
+	manageimage(PUT);
+
 	image->y = 100;
-	image->width = 100;
-	image->height = 100;
-	image->color = 0x00ffee;
-	manageimage(SQR);
-	image->color = 0xeeff00;
-	image->radius = 25;
-	manageimage(CIR);
+	image->x = 250;
+	image->yto = 20;
+	image->xto = 400;
+	image->color = 0x0000ff;
+	manageimage(LIN);
+	manageimage(PUT);
+
+	image->x = 200;
+	image->y = 210;
+	image->xto = 201;
+	image->yto = 300;
+	image->color = 0x00ff00;
+	manageimage(LIN);
 	manageimage(PUT);
 
 	image->x = 300;
 	image->y = 100;
-	image->xto = 50;
+	image->xto = 500;
 	image->yto = 250;
-	image->color = 0xeeffee;
+	image->color = 0xffffff;
 	manageimage(LIN);
 	manageimage(PUT);
+
+	// t_file *file = managefile(GET);
+	// int i;
+	// while (file)
+	// {
+	// 	i = 0;
+	// 	while (file->str[i])
+	// 	{
+	// 		while (ft_isspace(file->str[i]))
+	// 			i += 1;
+	// 		ft_printf("%d\n", ft_atoi(&file->str[i]));
+	// 		mlx_pixel_put(managemlx(GET), manageimage(GET), 10, 10, 0xff0000);
+	// 		i += 1;
+	// 	}
+	// 	file = file->next;
+	// }
+	// manageimage(PUT);
 
 	manageimagelist(PRINT);
 	mlx_loop(managemlx(GET));
